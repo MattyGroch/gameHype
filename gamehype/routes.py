1d2a5e35d6b96c9c6fe5feacd1cf6f83ea8eb3cd
Matt Grochocinski <matt.grochocinski@snapsheet.me>
2019-06-06 16:26:14 -0500

    Added index, updated routes to point to templates, updated requirements

--- gamehype/routes.py
+++ gamehype/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from gamehype import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Idiot! You did it!"
+    return render_template('index.html', title='Home')
