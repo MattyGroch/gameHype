7d78ff0ffb5d8fe8f62b3c696d3a02e52fdd850e
Matt Grochocinski <matt.grochocinski@gmail.com>
2019-06-07 15:57:36 -0500

    Made it work for real this time

--- gamehype/routes.py
+++ gamehype/routes.py
@@ -5,6 +5,6 @@ from gamehype.models import User, Rating
 @app.route('/')
 @app.route('/index')
 def index():
-    user = User.query.all()
-    rating = Rating.query.all()
-    return render_template('index.html', title='Home', user=user, rating=rating)
+    user = User.query.get(1)
+    ratings = Rating.query.all()
+    return render_template('index.html', title='Home', user=user, ratings=ratings)
