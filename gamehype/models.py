36da635c2f0cd4c65d33232bdb270dadc0e7abbb
Ben Podgorski <mepoddo@gmail.com>
2019-06-07 17:33:03 -0500

    add flask-login support

--- gamehype/models.py
+++ gamehype/models.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from gamehype import db
+from gamehype import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -11,6 +13,12 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class Rating(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     hype = db.Column(db.Integer)
@@ -18,4 +26,12 @@ class Rating(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Rating {}>'.format(self.rating)
+        return '<Rating {}>'.format(self.hype)
+
+class Platform(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    platform_name = db.Column(db.String(64), index=True, unique=True)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
