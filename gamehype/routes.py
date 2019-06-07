bcfcb5900865672f440e6e01d6554f5dab9a4b79
Matt Grochocinski <matt.grochocinski@gmail.com>
2019-06-07 15:48:01 -0500

    test pull from db

--- gamehype/routes.py
+++ gamehype/routes.py
@@ -1,18 +1,10 @@
 from flask import render_template
 from gamehype import app
+from gamehype.models import User, Rating
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Timmy Jimmy'}
-    ratings = [
-        {
-            'hypeman': {'username': 'Matt'},
-            'hype': '5/5'
-        },
-        {
-            'hypeman': {'username': 'Ben'},
-            'hype': '3/5'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, ratings=ratings)
+    user = User.query.all()
+    rating = Rating.query.all()
+    return render_template('index.html', title='Home', user=user, rating=rating)
