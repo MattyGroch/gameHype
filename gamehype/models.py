67df90474f34893b68919bc947ca6509b073fe1f
Ben Podgorski <mepoddo@gmail.com>
2019-06-07 14:01:27 -0500

    fixed naming conflict in pacakge, seeded db with dummy data

--- gamehype/models.py
+++ gamehype/models.py
@@ -6,14 +6,14 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    ratings = db.relationship('Rating', backref='owner', lazy='dynamic')
+    ratings = db.relationship('Rating', backref='hypeman', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
 class Rating(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    rating = db.Column(db.Integer)
+    hype = db.Column(db.Integer)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
---
+++ hypelist.py
@@ -0,0 +1,6 @@
+from gamehype import db, app
+from gamehype.models import User, Rating
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Rating': Rating}
