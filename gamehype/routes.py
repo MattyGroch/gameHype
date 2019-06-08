327b2e7d4b7806502d89038bcbec2b17ebfd1364
Matt Grochocinski <matt.grochocinski@gmail.com>
2019-06-07 23:05:09 -0500

    Added testing login page for frontend

--- gamehype/routes.py
+++ gamehype/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from gamehype import app
 from gamehype.models import User, Rating
+from gamehype.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -8,3 +9,12 @@ def index():
     user = User.query.get(1)
     ratings = Rating.query.all()
     return render_template('index.html', title='Home', user=user, ratings=ratings)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
