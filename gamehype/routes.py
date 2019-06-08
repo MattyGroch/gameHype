a398c20567443ca6635034607deedaac78370f61
Matt Grochocinski <matt.grochocinski@gmail.com>
2019-06-08 00:20:20 -0500

    Added working registration page!

--- gamehype/routes.py
+++ gamehype/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
-from gamehype import app
+from gamehype import app, db
 from gamehype.models import User, Rating
-from gamehype.forms import LoginForm
+from gamehype.forms import LoginForm, RegistrationForm
 
 
 @app.route('/')
@@ -16,6 +16,21 @@ def index():
         title='Home',
         ratings=ratings
         )
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
