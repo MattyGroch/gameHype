836b4c6b51a24da2f99549b3ffb2fdb2178a4bc0
Poddo <mepoddo@gmail.com>
2019-06-12 00:26:11 -0500

    add developer and publisher tables, brought name of platform check in line

--- gamehype/models.py
+++ gamehype/models.py
@@ -58,18 +58,21 @@ class Game(db.Model):
 
     genres = db.relationship('Genre', secondary="genre_games", lazy='dynamic')
 
+    developers = db.Relationship('Developer', secondary="developer_games", lazy='dynamic')
+    publishers = db.Relationship('Publisher', secondary="publisher_games", lazy='dynamic')
+
     def __repr__(self):
         return '<Game {}>'.format(self.game_name)
 
-    def on_platform(self, platform):
+    def check_platform(self, platform):
         return self.platforms.filter(platform_games.c.platform_id == platform.id).count() > 0
 
     def add_platform(self, platform):
-        if not self.on_platform(platform):
+        if not self.check_platform(platform):
             self.platforms.append(platform)
 
     def remove_platform(self, platform):
-        if self.on_platform(platform):
+        if self.check_platform(platform):
             self.platforms.remove(platform)
 
     def check_genre(self, genre):
@@ -83,6 +86,28 @@ class Game(db.Model):
         if self.check_genre(genre):
             self.genres.remove(genre)
 
+    def check_developer(self, developer):
+        return self.developers.filter(developer_games.c.developer_id == developer.id).count() > 0
+
+    def add_developer(self, developer):
+        if not self.check_developer(developer):
+            self.developers.append(developer)
+
+    def remove_developer(self, developer):
+        if self.check_developer(developer):
+            self.developers.remove(developer)
+
+    def check_publisher(self, publisher):
+        return self.publishers.filter(publisher_games.c.publisher_id == publisher.id).count() > 0
+
+    def add_publisher(self, publisher):
+        if not self.check_publisher(publisher):
+            self.publishers.append(publisher)
+
+    def remove_publisher(self, publisher):
+        if self.check_publisher(publisher):
+            self.publishers.remove(publisher)
+
 class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     genre_name = db.Column(db.String(64), index=True, unique=True)
@@ -92,6 +117,24 @@ class Genre(db.Model):
     def __repr__(self):
         return '<Genre {}>'.format(self.genre_name)
 
+class Developer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    developer_name = db.Column(db.String(128), index=True, unique=True)
+
+    games = db.relationship('Game', secondary="developer_games", lazy='dynamic')
+
+    def __repr__(self):
+        return '<Developer {}>'.format(self.developer_name)
+
+class Publisher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    publisher_name = db.Column(db.String(128), index=True, unique=True)
+
+    games = db.relationship('Game', secondary="publisher_games", lazy='dynamic')
+
+    def __repr__(self):
+        return '<Publisher {}>'.format(self.publisher_name)
+
 platform_games = db.Table('platform_games',
     db.Column('platform_id', db.Integer, db.ForeignKey('platform.id')),
     db.Column('game_id', db.Integer, db.ForeignKey('game.id'))
@@ -99,6 +142,16 @@ platform_games = db.Table('platform_games',
 
 genre_games = db.Table('genre_games',
     db.Column('genre_id', db.Integer, db.ForeignKey('genre.id')),
+    db.Column('game_id', db.Integer, db.ForeignKey('game.id'))
+    )
+
+developer_games = db.Table('developer_games',
+    db.Column('developer_id', db.Integer, db.ForeignKey('developer.id')),
+    db.Column('game_id', db.Integer, db.ForeignKey('game.id'))
+    )
+
+publisher_games = db.Table('publisher_games',
+    db.Column('publisher_id', db.Integer, db.ForeignKey('publisher.id')),
     db.Column('game_id', db.Integer, db.ForeignKey('game.id'))
     )
 
