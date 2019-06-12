47824d22ba47affeb45f17c66663347c8ec09ca2
Poddo <mepoddo@gmail.com>
2019-06-12 01:08:48 -0500

    added company class to flask shell

--- hypelist.py
+++ hypelist.py
@@ -1,7 +1,7 @@
 from gamehype import db, app
-from gamehype.models import User, Rating, Game, Platform
+from gamehype.models import User, Rating, Game, Platform, Genre, Company
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Rating': Rating, 'Game': Game, 'Platform': Platform}
+    return {'db': db, 'User': User, 'Rating': Rating, 'Game': Game, 'Platform': Platform, 'Genre': Genre, 'Company': Company}
